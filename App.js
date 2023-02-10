3cc64a03ed43c7beee669a7ca19624037c1523f0
Fernando Casais <fpcasais@gmail.com>
2023-02-09 23:43:00 -0300

    app react native

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, _Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 30}} >Hello Coders!!</Text>
+      <Text style={{color: 'red'}}>Hello people</Text>
+
+      <Text>WELCOME</Text>
+      <Text style={{fontSize: 30}}>to my app</Text>
+      <Text></Text>
+    
+      <Image
+  source={{
+    uri: 'https://www.quepelivemoshoy.com/wp-content/uploads/2020/11/poster-el-padrino-726x1024.jpg.webp',
+    method: 'POST',
+    headers: {
+      Pragma: 'no-cache'
+    },
+    body: 'Your Body goes here'
+  }}
+  style={{ width: 300, height: 400 }}
+/>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  textStyle: {
+    fontSize: 30,
+ 
+  
+    }
 });
